fd20a6879ce661940da1062bac578f2af58c53b2
Ayoub ABOUNAKIF <ayoubabounakif@gmail.com>
2021-10-15 15:24:56 +0100

    iterator_traits gay

--- random_access_iterator.hpp
+++ random_access_iterator.hpp
@@ -6,7 +6,7 @@
 /*   By: aabounak <aabounak@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/15 12:26:04 by aabounak          #+#    #+#             */
-/*   Updated: 2021/10/15 15:17:33 by aabounak         ###   ########.fr       */
+/*   Updated: 2021/10/15 15:24:38 by aabounak         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,13 @@ namespace ft {
     template < class T >
     class random_access_iterator : public iterator_traits< iterator<std::random_access_iterator_tag, T> > {
         public:
+                /*     When dealing with templates,
+                    you cannot separate the class declaration and class implementation into separate files
+                    (at least not without including the .cpp file at the end of the .h file).
+                    This is due to how templates are compiled.
+                    The simplest ways to do is to inline your functions. */
+
+                        /* These typedefs are the most stupid thing I've seen in life... */
             typedef typename iterator_traits< iterator<std::random_access_iterator_tag, T> >::value_type       value_type;
             typedef typename iterator_traits< iterator<std::random_access_iterator_tag, T> >::difference_type  difference_type;
 
@@ -27,25 +34,29 @@ namespace ft {
             inline random_access_iterator( const random_access_iterator& rhs ) : _data(rhs._data) {}
             inline random_access_iterator& operator= ( const random_access_iterator& rhs ) { this->_data = rhs._data; return (*this); }
             
-            //      Misc Operators
+                /* Misc Operators */
             inline random_access_iterator& operator+= ( difference_type rhs ) { this->_data += rhs; return *this; };
             inline random_access_iterator& operator-= ( difference_type rhs ) { this->_data -= rhs; return *this; };
             inline T& operator*() const { return *_data; }
             inline T* operator->() const { return _data; }
             inline T& operator[]( difference_type rhs ) { return (_data[rhs]); };
-    /*         TO-DO:           Some of these should be non-member functions */
-            //      Arithmetic Operators
+
+                        /* TO-DO: -- Some of these should be non-member functions */
+    
+                /* Arithmetic Operators */
             inline random_access_iterator& operator++() { ++_data; return *this; }
             inline random_access_iterator& operator--() { --_data; return *this; }
             inline random_access_iterator operator++( int ) const { random_access_iterator tmp(*this); ++_data; return tmp; }
             inline random_access_iterator operator--( int ) const { random_access_iterator tmp(*this); --_data; return tmp; }
-            /* difference_type operator+( const random_access_iterator& rhs ) const { return random_access_iterator(_data + rhs.ptr); }
+
+/*             difference_type operator+( const random_access_iterator& rhs ) const { return random_access_iterator(_data + rhs.ptr); }
             difference_type operator-( const random_access_iterator& rhs ) const { return random_access_iterator(_data - rhs.ptr); }
             random_access_iterator operator+( difference_type rhs ) const { return random_access_iterator(_data + rhs); }
             random_access_iterator operator-( difference_type rhs ) const { return random_access_iterator(_data - rhs); }
             random_access_iterator operator+( difference_type lhs, const random_access_iterator& rhs ) { return random_access_iterator(lhs + rhs._data); }
             random_access_iterator operator-( difference_type lhs, const random_access_iterator& rhs ) { return random_access_iterator(lhs - rhs._data); } */
-            //      Comparison Operators
+    
+                /* Comparison Operators */
             inline bool operator==( const random_access_iterator& rhs ) const { return _data == rhs._data; }
             inline bool operator!=( const random_access_iterator& rhs ) const { return _data != rhs._data; }
             inline bool operator>( const random_access_iterator& rhs ) const { return _data > rhs._data; }
