71ea4da17b33381bfe888d7745481a9bf9fcdce4
Ayoub ABOUNAKIF <ayoubabounakif@gmail.com>
2021-11-03 19:05:08 +0100

    the useless tree

--- GBT/GBT.hpp
+++ GBT/GBT.hpp
@@ -6,7 +6,7 @@
 /*   By: aabounak <aabounak@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/02 10:19:07 by aabounak          #+#    #+#             */
-/*   Updated: 2021/11/03 18:04:07 by aabounak         ###   ########.fr       */
+/*   Updated: 2021/11/03 19:03:51 by aabounak         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,60 +14,43 @@
 
 # include "../utility/pair.hpp"
 # include "../utility/make_pair.hpp"
+# include "../containers/vector.hpp"
 
 # define GREEN 420
 # define BLACK 42
 # define BLACK_BLACK 99
 
 namespace ft {
+    struct Node {
+        int     data;
+        Node  * parent;
+        Node  * left;
+        Node  * right;
+        short   color;      
+    };
+    
+    template < class T, class Alloc = std::allocator<T> >
     class GBT
     { 
         public:
+
+            typedef    T               value_type;
+            typedef    Alloc           allocator_type;
+
             // Constructor
-            GBT() { _root = NULL; }
-            ~GBT();
-
-            struct Node {
-                // Constructor
-                Node(int x) {
-                    this->data = x;
-                    this->parent = this->left = this->right = NULL;
-                    this->color = GREEN;
-                }
-                int     data;
-                Node  * parent;
-                Node  * left;
-                Node  * right;
-                short   color;      
-            };
-
-            /* A utility function to insert a new node with given key */
-            Node * insert(Node * pt) {
-                /* If the tree is empty, return a new node */
-                if (this->_root == NULL)
-                    return pt;
-                /* Otherwise, recur down the tree */
-                if (pt->data < this->_root->data) {
-                    this->_root->left = this->insert(pt);
-                    this->_root->left->parent = this->_root;
-                }
-                if (pt->data > this->_root->data) {
-                    this->_root->right = this->insert(pt);
-                    this->_root->right->parent = this->_root;
-                }
-                return this->_root;
-            }
-
-            void    inOrderTraversal(Node *node) {
-                (void)node;
-                if (_root == NULL)
-                    return ;
-                inOrderTraversal(_root->left);
-                std::cout << _root->data << " ";
-                inOrderTraversal(_root->right);
+            GBT() {
+                _TNULL = _alloc.allocate(1);
+                _TNULL->color = GREEN;
+                _TNULL->left = nullptr;
+                _TNULL->right = nullptr;
+                _root = _TNULL;
             }
             
+            ~GBT() { _alloc.deallocate(_TNULL, 1); }
+
         private:
-            Node *  _root;
+            Node    *       _root;
+            Node    *       _TNULL;
+            allocator_type  _alloc;
     }; 
 }
--- main.cpp
+++ main.cpp
@@ -6,20 +6,17 @@
 /*   By: aabounak <aabounak@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/06 12:23:28 by aabounak          #+#    #+#             */
-/*   Updated: 2021/11/03 18:02:57 by aabounak         ###   ########.fr       */
+/*   Updated: 2021/11/03 19:03:07 by aabounak         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-# include "containers/vector.hpp"
+// # include "containers/vector.hpp"
 // # include "containers/stack.hpp"
 # include "GBT/GBT.hpp"
 
 int main( void ) 
 {
-    ft::GBT	myBootyfulTree;
-
-	myBootyfulTree.insert(5);
-	std::cout << myBootyfulTree << std::endl;
+    
 }
 
 /* #if 1 //CREATE A REAL STL EXAMPLE
